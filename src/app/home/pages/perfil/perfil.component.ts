5bde68d7cbdd39bc9e63107e9e58e8deb5e16b19
Miqueas Gimenez <miquigz02@gmail.com>
2023-02-02 12:15:52 -0300

    feat: title desc animation

--- src/app/home/pages/perfil/perfil.component.ts
+++ src/app/home/pages/perfil/perfil.component.ts
@@ -1,9 +1,10 @@
 import { SvgsProfileService } from './../../services/svgs-profile.service';
 import { SocialSVG } from './../../interfaces/social-svg';
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 
 import Swal from 'sweetalert2';
 import { SVG_DISCORD } from '../../constants/profile-svgs/svg-discord.constant';
+import { timer, first } from 'rxjs';
 
 @Component({
   selector: 'app-perfil',
@@ -11,16 +12,25 @@ import { SVG_DISCORD } from '../../constants/profile-svgs/svg-discord.constant';
   styleUrls: ['./perfil.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
 
   //TODO: Hacer array
   svgDC = SVG_DISCORD;
+
+  private cargos: string[] = ['Fullstack', 'Frontend', 'Backend'];
+  private actualIndex:number = 0;
+
+  descDinamic:string = '';
   
   link:boolean[] = [false, false, false, false];
   socialItems:SocialSVG[] = [];
 
   constructor(svgsProfileService:SvgsProfileService) {
     this.socialItems = svgsProfileService.getAllSvgs();
+  }
+
+  ngOnInit(): void {
+    this.showName(this.cargos[this.actualIndex]);
   }
 
   showLink(num:number){
@@ -62,4 +72,44 @@ export class PerfilComponent {
       toast.addEventListener('mouseleave', Swal.resumeTimer)
     }
   })
+
+
+  showName(name:string){
+    const nameArray = name.split('');
+    for(let i = 0; i < nameArray.length; i++){
+      let time = 100 * i;
+      timer(time)
+      .pipe(first())
+      .subscribe(() => {
+        this.descDinamic += nameArray[i];
+      });
+    }
+
+    timer(100 * nameArray.length + 4000)
+    .pipe(first())
+    .subscribe(() => {
+      this.hideName();
+    });
+  }
+
+  hideName(){
+    const maxActual = this.descDinamic.length;
+    for(let i = maxActual - 1; i >= 0; i--) {
+      let time = 100 * i;
+      timer(time)
+      .pipe(first())
+      .subscribe(() => {
+        this.descDinamic = this.descDinamic.slice(0, -1);
+        if( i === maxActual - 1 ){
+          this.actualIndex++;
+          if(this.actualIndex === this.cargos.length){
+            this.actualIndex = 0;
+          }
+          timer(1000).pipe(first())
+          .subscribe(() => { this.showName(this.cargos[this.actualIndex]); });
+        }
+
+      });
+    }
+  }
 }
