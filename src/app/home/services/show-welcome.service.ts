19907b4d82d5b2763abb0df6caee4f0b779b4860
miquigz <miquigz02@gmail.com>
2023-03-05 10:59:28 -0300

    perf: behavior default value, service showWelcome

--- src/app/home/services/show-welcome.service.ts
+++ src/app/home/services/show-welcome.service.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class ShowWelcomeService {
 
-  private _hideWelcome:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  private _hideWelcome:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor() { }
 
